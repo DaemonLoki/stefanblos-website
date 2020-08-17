211f483a2f8eab11841c07f6dfde251a31d9f29b
Stefan Blos <stefan.blos@codecamp-n.com>
2020-08-17 22:32:50 +0200

    reworked UI of tags

--- Sources/StefanblosWebsite/Theme/Theme+ItemList.swift
+++ Sources/StefanblosWebsite/Theme/Theme+ItemList.swift
@@ -14,28 +14,19 @@ extension Node where Context == HTML.BodyContext {
         return .ul(
             .class("item-list"),
             .forEach(items) { item in
-                .li(.article(
+                .li(
                     createContent(for: item, on: site)
-                    )
                 )}
         )
     }
     
     static func createItemList<T: Website>(by id: T.SectionID, with items: [Item<T>], on site: T) -> Node {
-        print(id)
         guard let sectionId = id as? StefanBlosWebsite.SectionID else {
             return .div(.text("An error occurred. Mayday!"))
         }
         switch sectionId {
         case .posts:
-            return .ul(
-                .class("item-list"),
-                .forEach(items) { item in
-                    .li(.article(
-                        createContent(for: item, on: site)
-                        )
-                    )}
-            )
+            return itemList(for: items, on: site)
         case .about:
             return .aboutPage
         }
@@ -43,16 +34,18 @@ extension Node where Context == HTML.BodyContext {
     }
     
     private static func createContent<T: Website>(for item: Item<T>, on site: T) -> Node {
-        return .a(
-            .href(item.path),
-            .div(
-                .h1(.class("item-headline"),
-                    .text(item.title)
-                ),
-                .p(.text(item.description)),
-                .div(.class("bottom-container"),
-                     .tagList(for: item, on: site),
-                     .dateTime(for: item.date, className: "item-date")
+        return .article(
+            .a(
+                .href(item.path),
+                .div(
+                    .h1(.class("item-headline"),
+                        .text(item.title)
+                    ),
+                    .p(.text(item.description)),
+                    .div(.class("bottom-container"),
+                         .tagList(for: item, on: site),
+                         .dateTime(for: item.date, className: "item-date")
+                    )
                 )
             )
         )
--- Sources/StefanblosWebsite/Theme/Theme+TagList.swift
+++ Sources/StefanblosWebsite/Theme/Theme+TagList.swift
@@ -10,18 +10,15 @@ import Plot
 
 extension Node where Context == HTML.BodyContext {
     static func tagList<T: Website>(for item: Item<T>, on site: T) -> Node {
-        return .div(
-            .class("tag-list"),
-            .ul(
-                .forEach(item.tags) { tag in
+        return .ul(.class("tag-list"),
+                   .forEach(item.tags) { tag in
                     .li(
                         .a(
                             .href(site.path(for: tag)),
-                            .text(tag.string)
+                            .text("#" + tag.string)
                         )
                     )
-                }
-            )
+            }
         )
     }
 }
